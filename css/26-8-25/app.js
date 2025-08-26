24b832ea7c2d36ccfe52e6159b6c6a0e4f7bf3a1
Shahbaz Ahmed <ashabaz845@gmail.com>
2025-08-26 22:02:35 +0500

    javaScript

---
+++ css/26-8-25/app.js
@@ -0,0 +1,27 @@
+// alert("hello js") 
+
+
+// var username = "Apple"
+
+// console.log(typeof username)
+
+
+// var a = 2
+
+// console.log(typeof a)
+
+
+// var firstName = "Shahbaz"
+// var lastName = "Ahmed"
+// var fullName = firstName + " " +  lastName
+
+// console.log("Hi Welcome"+fullName)
+
+// var a= "12"
+// var b=4
+
+// console.log(a*b)
+
+var user = prompt("please Enter Your Name")
+
+console.log("user======>",user)
