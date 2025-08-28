5c7400194629a3975e8749d1ea74157fce05a122
Shahbaz Ahmed <ashabaz845@gmail.com>
2025-08-28 20:02:56 +0500

    JavaScript

---
+++ javaScript/28-8-25/app.js
@@ -0,0 +1,65 @@
+
+// var a = "2"
+// var b = Number(a)
+// console.log(typeof a)
+// console.log(typeof b)
+
+// var a = prompt("Enter number")
+// var b= Number(a)
+// console.log(typeof a)
+// console.log(typeof b)
+
+
+// var c = +prompt("Enter number")
+
+// console.log(typeof c)
+
+// var fullName = "Shahbaz Ahmed"
+// var hobbbies = "Game"
+// var educationn= "BSCS"
+
+// console.log("My Name is"+fullName+ "and My Hobbies is "+hobbbies + "and Eductaion is "+educationn )
+
+// var a = 3
+
+// console.log(a++)
+
+// console.log(a)
+
+// console.log(a++)
+// console.log(a)
+
+// var b = 6
+// console.log(++b)
+// console.log(++b)
+// console.log(b)
+
+var a = 4
+var b= 6
+
+var c = b++ + a++ + ++b + a++ + ++a + ++a + ++b 
+  //   
+
+  console.log(c)
+
+//52
+
+// console.log(6 == "7") //True
+
+// console.log( 6 === "6")
+
+
+// console.log(10>6 && 8 >10)
+
+// console.log(5<3 && 10<9)
+
+// console.log(7>8 || 6>4)
+
+
+// console.log((7>8 && 6<5) || (5>8 || 6<3))
+
+
+// console.log((2>1 || 6>3) && (6>9 && 5<4))
+
+
+console.log(((6<9 && 7>5) || (5>9 && 6<3)) && (8>9 && 5<3))
